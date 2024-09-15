96cc6103268a861ede715c56c41f68962f4f979a
Jalal Hejazi <jazj@demant.com>
2024-09-15 17:31:28 +0200

    devbox/pwsh

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -1,4 +1,5 @@
 
 # This is a test script to test the PowerShell script execution
 # pwsh.exe -File <FileName> -ExecutionPolicy Bypass
-Write-Host -Object "Testing PowerShell script..." -ForegroundColor Green
+
+Write-Host -Object "Testing PowerShell script with version: $($host.Version)"  
